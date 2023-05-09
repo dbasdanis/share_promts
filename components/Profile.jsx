d9dfa6ccbc59b60f649cec79e98d73661e1ebc81
dbasdanis <dbasdanis97@gmail.com>
2023-05-09 17:22:55 +0200

    fix: changes on the files

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,4 +1,4 @@
-import PromptCard from './PromptCard';
+import PromptCard from "./PromptCard";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
@@ -8,18 +8,18 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
       </h1>
       <p className='desc text-left'>{desc}</p>
 
-      <div className="mt-10 prompt_layout">
+      <div className='mt-10 prompt_layout'>
         {data.map((post) => (
           <PromptCard
             key={post._id}
             post={post}
             handleEdit={() => handleEdit && handleEdit(post)}
-            handleDelete={() => handleDelete(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
           />
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
